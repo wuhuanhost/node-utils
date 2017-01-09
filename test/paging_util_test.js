284e6f19af31727538b82dbccf419763c3df1f68
wuhuanhost <post.thinking@gmail.com>
2017-01-09 19:11:33 +0800

    update

--- test/paging_util_test.js
+++ test/paging_util_test.js
@@ -6,11 +6,11 @@ exports.test = function(cb) {
     var pageing = new page(); //实例化分页对象
     pageing
         .setDebug(true) //开启调试模式
-        .setCurrentPage(2) //显示第2页
-        .setPageSize(2) //每页显示2条
-        .setTable("books") //books表
-        .setOrderBy("ID") //排序字段
-        .setSortBy("ASC") //升序排列
+        .setCurrentPage(2) //显示第2页，默认显示第一页
+        .setPageSize(2) //每页显示2条，默认显示10条
+        .setTable("books") //books表，必须设置
+        .setOrderBy("ID") //排序字段，可以不设置
+        .setSortBy("ASC") //升序排列，可以不设置
         .paging(function(err, result) {
             console.log(result)
             cb(null, result)
