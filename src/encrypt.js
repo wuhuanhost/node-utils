654e5a6e3a57b110e1250b9a05d43f190918a73e
wuhuanhost <post.thinking@gmail.com>
2017-08-14 10:13:57 +0800

    添加了加密相关方法的文件

--- src/encrypt.js
+++ src/encrypt.js
@@ -11,3 +11,12 @@ exports.MD5 = function(str) {
     hash.update(str);
     return hash.digest('hex');
 }
+
+
+//sha1加密
+function sha1(str) {
+    var sha1 = crypto.createHash('sha1');
+    sha1.update(str);
+    var sha1Str = sha1.digest("hex");
+    return sha1Str;
+}
