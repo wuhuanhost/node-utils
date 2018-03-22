93a1036e310522011cdb31aae60afa385b3fdb67
lance0512 <post.thinking@gmail.com>
2018-03-22 15:52:47 +0800

    添加了新的工具类文件

---
+++ src/utils/base.js
@@ -0,0 +1,12 @@
+/**
+** 计算字符串的长度，中文每个字符长度为2英文每个字符长度为1
+**/
+var GetWordLength = function (str) {
+    var realLength = 0, len = str.length, charCode = -1;
+    for (var i = 0; i < len; i++) {
+        charCode = str.charCodeAt(i);
+        if (charCode >= 0 && charCode <= 128) realLength += 1;
+        else realLength += 2;
+    }
+    return realLength;
+};
