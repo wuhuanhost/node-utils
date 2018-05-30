da7fe200797b2aa9b839b5c009d1c4ee2a6bcc47
lance0512 <post.thinking@gmail.com>
2018-05-30 16:40:37 +0800

    修改了readme

--- src/utils/AipSpeechClient.js
+++ src/utils/AipSpeechClient.js
@@ -1,9 +1,9 @@
 var AipSpeechClient = require("baidu-aip-sdk").speech;
 
 // 设置APPID/AK/SK
-var APP_ID = "21167806";
-var API_KEY = "VIa7Oty5sQHsIHuEQGmNuxffa";
-var SECRET_KEY = "SEQn77Ls3iCCXmvM9ag8n1iYf8mVNNsbc";
+var APP_ID = "11167806";
+var API_KEY = "VIa7Oty5sQHsIHuEQGmNuxff";
+var SECRET_KEY = "SEQn77Ls3iCCXmvM9ag8n1iYf8mVNNsb";
 
 // 新建一个对象，建议只保存一个对象调用服务接口
 var client = new AipSpeechClient(APP_ID, API_KEY, SECRET_KEY);
