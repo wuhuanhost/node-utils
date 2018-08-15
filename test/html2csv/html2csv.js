ad7c2cec5cc2230a0e0f7517602330da6d245e5c
lance0512 <post.thinking@gmail.com>
2018-08-15 14:19:06 +0800

    update

--- test/html2csv/html2csv.js
+++ test/html2csv/html2csv.js
@@ -14,12 +14,12 @@ const Json2csvParser = require('json2csv').Parser;
 function readHtml(path, cb) {
     var html = fs.readFile(path, function(err, data) {
         if (!err) {
-            cb(data.toString())
+            cb(data.toString());
         } else {
             console.error(err);
         }
     });
-}
+};
 
 /**
  * 解析需要的数据
@@ -68,23 +68,23 @@ function parseHtml(html) {
 
     //转换为cvs
     const fields = ['漏洞标题', '漏洞等级', '漏洞.详细描述', '漏洞.解决办法', '漏洞.威胁分值', '漏洞.危险插件', '漏洞.发现日期', '漏洞.CVE编号', '漏洞.BUGTRAQ', '漏洞.NSFOCUS', '漏洞.CNNVD编号', '漏洞.CNCVE编号', '漏洞.CNVD编号'];
-    var jsonArr = {
-        '漏洞标题': 'PHP Calendar扩展数字错误漏洞(CVE-2015-1353)',
-        '漏洞等级': '高危险',
-        '漏洞': {
-            '详细描述': 'PHP（PHP：HypertextPreprocessor，PHP：超文本预处理器）是PHPGroup和开放源代码社区共同维护的一种开源的通用计算机脚本语言。 calendar是其中的一个基于Web的日历事务系统扩展插件。\ r\ nPHP5 .6 .7 及之前版本的calendar扩展中存在整数溢出漏洞， 该漏洞源于gregor.c文件中的 & aposGregorianToSdn & apos函数和julian.c文件中的 & aposJulianToSdn & apos函数没有充分过滤特制的 & aposyear & apos值。 远程攻击者可利用该漏洞造成拒绝服务。 ',
-            '解决办法': '厂商补丁:\r\n目前厂商已经发布了升级补丁以修复此安全问题，补丁获取链接：\r\nhttp://php.net/ChangeLog-5.php',
-            '威胁分值': '7.0',
-            '危险插件': '否',
-            '发现日期': '2015-03-30',
-            'CVE编号': '编号：CVE-2015-1353\r\n\r\n参考链接：http://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2015-1353',
-            BUGTRAQ: '编号：95151\r\n\r\n参考链接：http://www.securityfocus.com/bid/95151',
-            NSFOCUS: '编号：35760\r\n\r\n参考链接：http://www.nsfocus.net/vulndb/35760',
-            'CNNVD编号': '编号：CNNVD-201501-606\r\n\r\n参考链接：http://www.cnnvd.org.cn/vulnerability/show/cv_cnnvdid/CNNVD-201501-606',
-            'CNCVE编号': 'CNCVE-20151353',
-            'CNVD编号': '编号：CNVD-2015-02111\r\n\r\n参考链接：http://www.cnvd.org.cn/flaw/show/CNVD-2015-02111'
-        }
-    }
+    // var jsonArr = {
+    //     '漏洞标题': 'PHP Calendar扩展数字错误漏洞(CVE-2015-1353)',
+    //     '漏洞等级': '高危险',
+    //     '漏洞': {
+    //         '详细描述': 'PHP（PHP：HypertextPreprocessor，PHP：超文本预处理器）是PHPGroup和开放源代码社区共同维护的一种开源的通用计算机脚本语言。 calendar是其中的一个基于Web的日历事务系统扩展插件。\ r\ nPHP5 .6 .7 及之前版本的calendar扩展中存在整数溢出漏洞， 该漏洞源于gregor.c文件中的 & aposGregorianToSdn & apos函数和julian.c文件中的 & aposJulianToSdn & apos函数没有充分过滤特制的 & aposyear & apos值。 远程攻击者可利用该漏洞造成拒绝服务。 ',
+    //         '解决办法': '厂商补丁:\r\n目前厂商已经发布了升级补丁以修复此安全问题，补丁获取链接：\r\nhttp://php.net/ChangeLog-5.php',
+    //         '威胁分值': '7.0',
+    //         '危险插件': '否',
+    //         '发现日期': '2015-03-30',
+    //         'CVE编号': '编号：CVE-2015-1353\r\n\r\n参考链接：http://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2015-1353',
+    //         BUGTRAQ: '编号：95151\r\n\r\n参考链接：http://www.securityfocus.com/bid/95151',
+    //         NSFOCUS: '编号：35760\r\n\r\n参考链接：http://www.nsfocus.net/vulndb/35760',
+    //         'CNNVD编号': '编号：CNNVD-201501-606\r\n\r\n参考链接：http://www.cnnvd.org.cn/vulnerability/show/cv_cnnvdid/CNNVD-201501-606',
+    //         'CNCVE编号': 'CNCVE-20151353',
+    //         'CNVD编号': '编号：CNVD-2015-02111\r\n\r\n参考链接：http://www.cnvd.org.cn/flaw/show/CNVD-2015-02111'
+    //     }
+    // }
 
     const myCars = jsonArr;
     const json2csvParser = new Json2csvParser({ fields, unwind: 'content', unwindBlank: true, encoding: 'utf-8' });
@@ -92,7 +92,7 @@ function parseHtml(html) {
 
     // console.log(csv);
     fs.writeFileSync('./a.csv', csv, { encoding: 'utf8' });
-}
+};
 
 
 var rege_match_a = /\<a target="_blank" href="(.+?)"\>(.+?)\<\/a\>/gi;
