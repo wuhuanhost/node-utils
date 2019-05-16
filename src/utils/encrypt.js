abcf9001ff5fd367fcbe295033ee3a736f18b2fa
wuhuanhost <post.thinking@gmail.com>
2019-05-16 11:29:21 +0800

    '修改了加密算法'

--- src/utils/encrypt.js
+++ src/utils/encrypt.js
@@ -27,10 +27,11 @@ function sha1(str) {
  */
 function aes256EncodeCBC(data, key, iv) {
     var encrypt = CryptoJS.AES.encrypt(data, CryptoJS.enc.Utf8.parse(key), {
-        iv: iv,
-        mode: CryptoJS.mode.ECB,
-        padding: CryptoJS.pad.Pkcs7,
-        format: CryptoJS.format.Hex //CBC模式需要加这个变量
+        iv: CryptoJS.enc.Utf8.parse(iv), //CBC模式需要格式化
+        // iv: iv,
+        mode: CryptoJS.mode.CBC,
+        padding: CryptoJS.pad.Pkcs7
+        // format: CryptoJS.format.Hex //返回的数据格式
     });
     // console.log("value: " + encrypt);
     return encrypt.toString();
@@ -44,18 +45,22 @@ function aes256EncodeCBC(data, key, iv) {
  */
 function aes256DecodeCBC(data, key, iv) {
     var decrypt = CryptoJS.AES.decrypt(data, CryptoJS.enc.Utf8.parse(key), {
-        iv: iv,
-        mode: CryptoJS.mode.ECB,
-        padding: CryptoJS.pad.Pkcs7,
-        format: CryptoJS.format.Hex
+        iv: CryptoJS.enc.Utf8.parse(iv),
+        // iv: iv,
+        mode: CryptoJS.mode.CBC,
+        padding: CryptoJS.pad.Pkcs7
+        // format: CryptoJS.format.Hex
     });
     // console.log("value: " + decrypt.toString(CryptoJS.enc.Utf8));
     return decrypt.toString(CryptoJS.enc.Utf8);
 }
 
-const iv = crypto.randomBytes(16);
-var key = MD5(iv);
-console.log("TCL: key", key.length);
+// 向量（向量长度为16,24,32对应AES128,AES192,AES256）目前好像只能使用AES128位加密
+const iv = "1234567890000000";
+console.log("TCL: iv", iv);
+// 秘钥
+var key = "1234567890000000";
+console.log("TCL: key", key);
 console.log("AES加密解密测试");
 var data = "123456789";
 console.log("加密字符串==>:" + data);
@@ -65,53 +70,68 @@ console.log(serctData);
 console.log("解密后的结果=====>");
 console.log(aes256DecodeCBC(serctData, key, iv));
 
-console.log();
-
 exports.MD5 = MD5;
 
-console.log("-----------------------");
-const NodeRSA = require("node-rsa");
+/**
+ * RSA测试
+ */
+function RSATest() {
+    console.log("-----------------------RSA加密解密测试-------------------------------------------");
+    const NodeRSA = require("node-rsa");
 
-pkcsType = "pkcs8"; //不为空则 设置为传入参数，为空则 设置为 pkcs8
-console.log("pkcsType=" + pkcsType);
+    pkcsType = "pkcs8"; //不为空则 设置为传入参数，为空则 设置为 pkcs8,常用pkcs8和pkcs1
+    console.log("pkcsType=" + pkcsType);
 
-// 1.创建RSA对象，并指定 秘钥长度
-const key1 = new NodeRSA({ b: 512 });
-key1.setOptions({ encryptionScheme: "pkcs1" }); //指定加密格式
+    // 1.创建RSA对象，并指定 秘钥长度
+    const key1 = new NodeRSA({ b: 2048 }); //秘钥长度512,1024,2048越长越安全
+    key1.setOptions({ encryptionScheme: "pkcs1" }); //指定加密格式
 
-// 2.生成 公钥私钥，使用 pkcs8标准，pem格式
-var publicPem = key1.exportKey(pkcsType + "-public-pem"); //制定输出格式
-var privatePem = key1.exportKey(pkcsType + "-private-pem");
-//console.log(key.$options);
-console.log(pkcsType + "公钥:\n", publicPem);
-console.log(pkcsType + "私钥:\n", privatePem);
+    // 2.生成 公钥私钥，使用 pkcs8标准，pem格式
+    var publicPem = key1.exportKey(pkcsType + "-public-pem"); //制定输出格式
+    var privatePem = key1.exportKey(pkcsType + "-private-pem");
+    //console.log(key.$options);
+    console.log(pkcsType + "公钥:\n", publicPem);
+    console.log(pkcsType + "私钥:\n", privatePem);
 
-var encryData = key1.encryptPrivate(data, "base64", "utf8");
-console.log("\n私钥加密后的数据：\n", encryData); //加密后数据为 base64 编码
+    var encryData = key1.encryptPrivate(data, "base64", "utf8");
+    console.log("\n私钥加密后的数据：\n", encryData); //加密后数据为 base64 编码
 
-// 4.使用 公钥 解密 数据，并指定字符集
-var decryptData = key1.decryptPublic(encryData, "utf8");
-console.log("\n公钥解密后的数据：\n", decryptData);
+    // 4.使用 公钥 解密 数据，并指定字符集
+    var decryptData = key1.decryptPublic(encryData, "utf8");
+    console.log("\n公钥解密后的数据：\n", decryptData);
 
-console.log("==================================================================1");
-// 1. 公钥
-var publicKey = new NodeRSA({ b: 512 });
+    console.log("==================================================================1");
+    // 1. 公钥
+    var publicKey = new NodeRSA({ b: 512 });
 
-// 2.导入 公钥，并指定使用 pkcs标准，pem格式
-publicKey.importKey(publicPem, pkcsType + "-public-pem");
+    // 2.导入 公钥，并指定使用 pkcs标准，pem格式
+    publicKey.importKey(publicPem, pkcsType + "-public-pem");
 
-// 3.使用 公钥 解密数据
-var decrypted = publicKey.decryptPublic(encryData, "utf8");
-console.log("\n使用公钥解密后的数据：\n", decrypted);
+    // 3.使用 公钥 解密数据
+    var decrypted = publicKey.decryptPublic(encryData, "utf8");
+    console.log("\n使用公钥解密后的数据：\n", decrypted);
 
-//---------------------demo3：服务端使用私钥签名------------------------
+    //---------------------demo3：服务端使用私钥签名------------------------
 
-// 1. 私钥
-var privateKey = new NodeRSA({ b: 512 });
+    // 1. 私钥
+    var privateKey = new NodeRSA({ b: 512 });
 
-// 2.导入 私钥，并指定使用 pkcs标准，pem格式
-privateKey.importKey(privatePem, pkcsType + "-private-pem");
+    // 2.导入 私钥，并指定使用 pkcs标准，pem格式
+    privateKey.importKey(privatePem, pkcsType + "-private-pem");
 
-var signedData = privateKey.sign(Buffer.from(data), "base64").toString("base64");
+    var signedData = privateKey.sign(Buffer.from(data), "base64").toString("base64");
 
-console.log("\n使用私钥签名:", signedData);
+    console.log("\n使用私钥签名:", signedData);
+
+    //---------------------demo4：服务端使用公钥验证签名---------------------
+
+    var result = publicKey.verify(Buffer.from(data), signedData, "utf8", "base64");
+
+    console.log("\n验证签名结果", result);
+}
+
+// RSATest();
+// SHA256 算法
+console.log("----------------------------------------------------sha256------------------------------");
+console.log(CryptoJS.HmacSHA256("111", "111").toString());
+console.log(CryptoJS.SHA256("111").toString());
